fd4f61d8b968ece9745d29521489460738bb3cba
ivett Verebélyi <paulusz.zsombor@gmail.com>
2023-02-16 19:57:45 +0100

    törlés gomb add

--- src/app.js
+++ src/app.js
@@ -1,22 +1,38 @@
-var szam = "35";
-var szam2 = '27';
 
-console.log(Number(szam) + Number(szam2));
+const gyumolcsok = [
+    { name: 'szilva', quantity: 35, price: 8 },
+    { name: 'alma', quantity: 45, price: 8.3 },
+    { name: 'körte', quantity: 25, price: 9.5 },
+    { name: 'barack', quantity: 37, price: 12 }
+];
+const tbody = document.querySelector("#tbody");
 
-var esik = true;
-if (esik) {
-    console.log('esik....')
+function generateTbody() {
+    gyumolcsok.forEach((gyumolcs) => {
+        let tr = document.createElement('tr');
+        let tdName = document.createElement('td');
+        let tdQuantity = document.createElement('td');
+        let tdPrice = document.createElement('td');
 
-    function calcArea(base, height) {
-        return base * height /2
-    }
+        tdName.textContent = gyumolcs.name;
+        tdQuantity.textContent = gyumolcs.quantity;
+        tdPrice.textContent = gyumolcs.price;
+
+        tbody.append(tr);
+        tr.append(tdName);
+        tr.append(tdQuantity);
+        tr.append(tdPrice);
+        tr.append(generateTdDelete());
+
+    });
 }
-console.log(typeof calcArea)
+generateTbody();
 
-var pista = {
-    nev:'pisti',
-    kor: 28
-};
-console.log(typeof pista)
-var szamok = [];
-console.log(typeof szamok);
+function generateTdDelete() {
+    let td = document.createElement('td');
+    let button = document.createElement('button');
+    button.textContent = "Törlés";
+    button.classList = "btn btn-warning";
+    td.append(button);
+    return td;
+}
